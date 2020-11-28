741a102725da6099516f0d7d53eedd2f6c8b06f0
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2020-11-28 16:15:59 +1100

    initial setup

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Mullish',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  font-weight: 300;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+h1, h2, h3, h4 {
+  font-family:
+    'Poppins',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-weight: 600;
+}
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
